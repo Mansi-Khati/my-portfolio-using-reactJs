a66dfcfc7aae7eedb1434a8bbc47de31038a546a
Mansi Khati <mansi2020cs114@abesit.edu.in>
2023-07-15 11:34:48 +0530

    now anchor tag is the whole div rather than just the text

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -8,36 +8,40 @@ function Contact() {
    return (
       <>
          <h2 className="head-text">Let's Connect!!</h2>
+         
          <div className="app__footer-cards">
-            <div className="app__footer-card">
-               <img src={images.email} alt="email"/>
-               <a href="mailto:khatimansi2004@gmail.com" className="p-text">
+
+            <a href="mailto:khatimansi2004@gmail.com" className="p-text">
+               <div className="app__footer-card">
+                  <img src={images.email} alt="email"/>
                   Email
-               </a>
-            </div>
+               </div>
+            </a>
             
-            <div className="app__footer-card">
             
-               <img src={images.twitter} alt="twitter"/>
-               <a href="https://twitter.com/mansi_khati" className="p-text">
-               Twitter
-               </a>
-            </div>
-            <div className="app__footer-card">
-               <img src={images.linkedln} alt="linkedin"/>
-               <a
-                  href="https://www.linkedin.com/in/mansi-khati-160bb5214"
-                  className="p-text"
-               >
+            <a href="https://twitter.com/mansi_khati" className="p-text">
+               <div className="app__footer-card">
+                  <img src={images.twitter} alt="twitter"/>
+                  Twitter
+               </div>
+            </a>
+            
+            
+            <a href="https://www.linkedin.com/in/mansi-khati-160bb5214" className="p-text">
+               <div className="app__footer-card">
+                  <img src={images.linkedln} alt="linkedin"/>
                   Linkedln
-               </a>
-            </div>
-            <div className="app__footer-card">
-               <img src={images.github} alt="github"/>
-               <a href="https://github.com/Mansi-Khati" className="p-text">
-                 GitHub
-               </a>
-            </div>
+               </div>
+            </a>
+            
+            
+            <a href="https://github.com/Mansi-Khati" className="p-text">
+               <div className="app__footer-card">
+                  <img src={images.github} alt="github"/>
+                  GitHub
+               </div>
+            </a>
+            
          </div>
       </>
    );
